953aedc50adc40f8f7c7d04823596c9973e612fe
user1 <user1@DESKTOP-TK4HNHS>
2023-07-07 10:23:11 +0900

    초기화

---
+++ Algorithm/src/baegjun/Baegjun_2468.java
@@ -0,0 +1,13 @@
+package baegjun;
+
+public class Baegjun_2468 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Algorithm/src/baegjun/Baekjoon.java
@@ -0,0 +1,24 @@
+package baegjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Baekjoon {
+
+	public static void main(String[] args)throws IOException {
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+		
+		br.readLine();
+	}
+
+}
